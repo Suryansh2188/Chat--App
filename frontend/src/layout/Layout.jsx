23aac7c415c47dbf9171744d4c2983ae6a9de1c3
Suryansh2188 <suryamail4440@gmail.com>
2024-10-29 21:51:49 +0530

    Redirect if currentUser's username does not match params.username

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,16 +1,32 @@
 import ChatBox from "../components/ChatBox/ChatBox"
 import ChatList from "../components/ChatList/ChatList"
 import Header from "../components/Header/Header"
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function Layout() {
     const { currentUser } = useContext(AuthContext);
+    const params = useParams();
+    console.log(params.username);
+
+    // useEffect(() => {
+    //     if (currentUser.username !== params.username) return alert("Not")
+    // }, [currentUser, params.username])
+    if (currentUser) {
+        if ((currentUser.username !== params.username)) {
+            return <Navigate to={`/${currentUser.username}`} />
+        }
+    }
+
+
+
     return (
         !currentUser ? <Navigate to='/' /> : <div className="w-full bg-slate-800 p-1 min-h-screen">
             <Header />
-            <div className="w-full lg:flex  justify-center items-center gap-1 mt-1">
+            <div className="w-full lg:flex justify-center items-center gap-1 mt-1">
                 {/* <div className="lg:w-1/4 w-full bg-green-600 lg:h-[34.3rem] flex justify-center items-center rounded-2xl">
                 <h1 className="text-7xl">Chat List</h1>
             </div> */}
@@ -18,5 +34,5 @@ export default function Layout() {
                 <ChatBox />
             </div>
         </div>
-    )
+    );
 }
